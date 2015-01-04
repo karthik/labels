f380728a2b45cd00b13c92a6db615f832cd04e12
Karthik Ram <karthik.ram@gmail.com>
2015-01-04 15:59:11 -0800

    Updated label commands

---
+++ labels.sh
@@ -0,0 +1,16 @@
+# This clears all existing labels
+ghi label -D bug
+ghi label -D duplicate 
+ghi label -D enhancement 
+ghi label -D invalid 
+ghi label -D question
+ghi label -D wontfix
+ghi label -D 'help wanted'
+
+# This creates a new label
+
+ghi label todo -c "#2980b9"
+
+
+
+
